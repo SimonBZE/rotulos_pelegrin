a8dabebd92fab719775d198287a87e118b5efe7e
byronsz <31295399+byronsz@users.noreply.github.com>
2023-08-17 02:26:18 +0200

    Avanzando

---
+++ app/(root)/stock-levels/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StockLevels() {
+  return (
+    <div>StockLevels</div>
+  )
+}
--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -58,9 +58,9 @@ const Header = ({
 
           <Link className="block flex-shrink-0 lg:hidden" href="/">
             <Image
-              width={32}
+              width={100}
               height={32}
-              src={"/images/logo/logo-icon.svg"}
+              src={"/assets/oraculo_oscuro.svg"}
               alt="Logo"
             />
           </Link>
@@ -105,7 +105,7 @@ const Header = ({
         <div className="flex items-center gap-3 2xsm:gap-7">
           <ul className="flex items-center gap-2 2xsm:gap-4">
             {/* <!-- Dark Mode Toggler --> */}
-            <DarkModeSwitcher />
+            {/* <DarkModeSwitcher /> */}
             {/* <!-- Dark Mode Toggler --> */}
 
             {/* <!-- Notification Menu Area --> */}
