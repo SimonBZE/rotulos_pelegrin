28608eaef014dc6c56d8ac2b34e44241aab732d4
byronsz <31295399+byronsz@users.noreply.github.com>
2024-05-14 19:18:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,6 +18,12 @@ const nextConfig = {
           port: '3000',
           // pathname: '',
         },
+        {
+          protocol: 'http',
+          hostname: 'https://rotulos-pelegrin-git-alpha-rotulos-pelegrin.vercel.app',
+          port: '3000',
+          // pathname: '',
+        },
       ],
       formats: ['image/avif', 'image/webp'],
     },
