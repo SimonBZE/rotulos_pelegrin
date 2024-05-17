9a4f52d15fec6cae0263f492704e986a378a1cde
byronsz <31295399+byronsz@users.noreply.github.com>
2024-05-17 17:11:40 +0200

    cambios para vercel

--- server.js
+++ server.js
@@ -1,4 +1,38 @@
-// server.js
+// // server.js
+// const { createServer } = require('http');
+// const next = require('next');
+// const { parse } = require('url');
+// const SocketIO = require('socket.io');
+
+// const dev = process.env.NODE_ENV !== 'production';
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+
+// app.prepare().then(() => {
+//   const server = createServer((req, res) => {
+//     const parsedUrl = parse(req.url, true);
+//     handle(req, res, parsedUrl);
+//   });
+
+//   const io = SocketIO(server);
+
+//   io.on('connection', (socket) => {
+//     console.log('New client connected');
+
+//     socket.on('disconnect', () => {
+//       console.log('Client disconnected');
+//     });
+//   });
+
+//   server.listen(3000, (err) => {
+//     if (err) throw err;
+//     console.log('> Ready on http://127.0.0.1:3000');
+//   });
+
+//   // Hacer que io esté disponible globalmente
+//   global.io = io;
+// });
+
 const { createServer } = require('http');
 const next = require('next');
 const { parse } = require('url');
@@ -24,11 +58,12 @@ app.prepare().then(() => {
     });
   });
 
-  server.listen(3000, (err) => {
+  server.listen(process.env.PORT || 3000, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://127.0.0.1:3000');
+    console.log('> Ready on http://localhost:3000');
   });
 
   // Hacer que io esté disponible globalmente
   global.io = io;
 });
+
