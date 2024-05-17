1e627f69756f300990ef7717f02fa82c92677f10
byronsz <31295399+byronsz@users.noreply.github.com>
2024-05-17 19:03:49 +0200

    retocediendo errores

--- server.js
+++ server.js
@@ -1,37 +1,3 @@
-// // server.js
-// const { createServer } = require('http');
-// const next = require('next');
-// const { parse } = require('url');
-// const SocketIO = require('socket.io');
-
-// const dev = process.env.NODE_ENV !== 'production';
-// const app = next({ dev });
-// const handle = app.getRequestHandler();
-
-// app.prepare().then(() => {
-//   const server = createServer((req, res) => {
-//     const parsedUrl = parse(req.url, true);
-//     handle(req, res, parsedUrl);
-//   });
-
-//   const io = SocketIO(server);
-
-//   io.on('connection', (socket) => {
-//     console.log('New client connected');
-
-//     socket.on('disconnect', () => {
-//       console.log('Client disconnected');
-//     });
-//   });
-
-//   server.listen(3000, (err) => {
-//     if (err) throw err;
-//     console.log('> Ready on http://127.0.0.1:3000');
-//   });
-
-//   // Hacer que io esté disponible globalmente
-//   global.io = io;
-// });
 
 const { createServer } = require('http');
 const next = require('next');
@@ -58,13 +24,48 @@ app.prepare().then(() => {
     });
   });
 
-  server.listen(process.env.PORT || 3000, (err) => {
+  server.listen(3000, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log('> Ready on http://127.0.0.1:3000');
   });
 
   // Hacer que io esté disponible globalmente
   global.io = io;
 });
+
+// const { createServer } = require('http');
+// const next = require('next');
+// const { parse } = require('url');
+// const SocketIO = require('socket.io');
+
+// const dev = process.env.NODE_ENV !== 'production';
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+
+// app.prepare().then(() => {
+//   const server = createServer((req, res) => {
+//     const parsedUrl = parse(req.url, true);
+//     handle(req, res, parsedUrl);
+//   });
+
+//   const io = SocketIO(server);
+
+//   io.on('connection', (socket) => {
+//     console.log('New client connected');
+
+//     socket.on('disconnect', () => {
+//       console.log('Client disconnected');
+//     });
+//   });
+
+//   server.listen(process.env.PORT || 3000, (err) => {
+//     if (err) throw err;
+//     console.log('> Ready on http://localhost:3000');
+//   });
+
+//   // Hacer que io esté disponible globalmente
+//   global.io = io;
+// });
+
 
 
