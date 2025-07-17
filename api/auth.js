0faea3f5f0cb979d9700c06a6cad71b2c73c49db
byronsz <simonbze@gmail.com>
2025-07-17 23:10:19 +0200

    test

--- api/auth.js
+++ api/auth.js
@@ -2,26 +2,27 @@ import {ENV} from '@/utils/constants'
 
 export class Auth{
     async login(data) {
-        try{
-            const url = `${ENV.API_URL}/${ENV.ENDPOINTS.AUTH.LOGIN}`
-            const params = {
-                method: 'POST',
-                headers:{
-                    'Content-Type':'application/json',
-                },
-                body: JSON.stringify(data),
-            };
+        // try{
+        //     const url = `${ENV.API_URL}/${ENV.ENDPOINTS.AUTH.LOGIN}`
+        //     const params = {
+        //         method: 'POST',
+        //         headers:{
+        //             'Content-Type':'application/json',
+        //         },
+        //         body: JSON.stringify(data),
+        //     };
 
-            const response = await fetch(url, params);
-            const result = await response.json();
+        //     const response = await fetch(url, params);
+        //     const result = await response.json();
             
-            if(response.status !== 200) throw result;
+        //     if(response.status !== 200) throw result;
 
-            return result;
+        //     return result;
 
-        }catch(error){
-            throw error;
-        }
+        // }catch(error){
+        //     throw error;
+        // }
+        return true
     }
 
     async resetPassword(data) {
